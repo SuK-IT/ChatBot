efdff2d1a145bdd7cb33a7e7bbf5abcdb0cb838e
Griefed <griefed@griefed.de>
2021-09-02 13:34:35 +0200

    Initial commit

---
+++ backend/test/java/de/sukit/chatbot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sukit.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
