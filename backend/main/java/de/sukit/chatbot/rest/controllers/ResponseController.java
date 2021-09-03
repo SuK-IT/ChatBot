15f63e53e9ba72afadc55def5d79f008c736ae22
Griefed <griefed@griefed.de>
2021-09-03 12:56:37 +0200

    feat: Return a response based on user-input recieved from our frontend via /api/talk/getResponse?input=<user_input>

---
+++ backend/main/java/de/sukit/chatbot/rest/controllers/ResponseController.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2021  SUK-IT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.sukit.chatbot.rest.controllers;
+
+import de.sukit.chatbot.DictionaryHandler;
+import de.sukit.chatbot.FileWatcher;
+import de.sukit.chatbot.rest.handlers.ResponseHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * RestController for responses from the dictionary based on the user-input recieved from the frontend.
+ * @author Griefed
+ */
+@RestController
+@RequestMapping("/api/talk")
+public class ResponseController {
+
+    private static final Logger LOG = LogManager.getLogger(ResponseController.class);
+
+    private final ResponseHandler RESPONSEHANDLER;
+    private final DictionaryHandler DICTIONARYHANDLER;
+    private final FileWatcher FILEWATCHER;
+
+    /**
+     * Constructor for our RestController. Ensures our dictionary is always up-to-date and filewatchers are present.
+     * @author Griefed
+     * @param injectedResponseHandler Instance of {@link ResponseHandler}.
+     * @param injectedDictionaryHandler Instance of {@link DictionaryHandler}.
+     * @param injectedFileWatcher Instance of {@link FileWatcher}:
+     */
+    @Autowired
+    public ResponseController(ResponseHandler injectedResponseHandler, DictionaryHandler injectedDictionaryHandler, FileWatcher injectedFileWatcher) {
+        this.RESPONSEHANDLER = injectedResponseHandler;
+        this.DICTIONARYHANDLER = injectedDictionaryHandler;
+        this.FILEWATCHER = injectedFileWatcher;
+    }
+
+    /**
+     * Getter the response from the dictionary based on the user-input recieved from the frontend.
+     * @author Griefed
+     * @param input User-input recieved from the frontend.
+     * @return String. Returns the response from the dictionary based on the user-input recieved from the frontend.
+     */
+    @GetMapping("/getResponse") // /api/talk/getResponse?input=wort0
+    public String getResponse(@RequestParam(value = "input", defaultValue = "hello") String input) {
+        LOG.info("Requested " + input);
+        return RESPONSEHANDLER.getReponse(input);
+    }
+
+}
---
+++ backend/main/java/de/sukit/chatbot/rest/handlers/ResponseHandler.java
@@ -0,0 +1,82 @@
+/* Copyright (C) 2021  SUK-IT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package de.sukit.chatbot.rest.handlers;
+
+import de.sukit.chatbot.DictionaryHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * ResponseHandler handles the input from our frontend and returns a response based on the input from the frontend.
+ * @author Griefed
+ */
+@Component
+public class ResponseHandler {
+
+    private static final Logger LOG = LogManager.getLogger(ResponseHandler.class);
+
+    private final DictionaryHandler DICTIONARYHANDLER;
+
+    private String response;
+    private boolean responseFound;
+
+    /**
+     * Constructor for the ResponseHandler which makes an instance of {@link DictionaryHandler} available to us, so we
+     * have access to our dictionary-hashmap.
+     * @author Griefed
+     * @param injectedDictionaryHandler Instance of {@link DictionaryHandler}.
+     */
+    @Autowired
+    public ResponseHandler(DictionaryHandler injectedDictionaryHandler) {
+        this.DICTIONARYHANDLER = injectedDictionaryHandler;
+    }
+
+    /**
+     * Get a response based on the input recieved from the user-input in the frontend.
+     * @author Griefed
+     * @param input String. The input recieved from the user-input in the frontend.
+     * @return String. Returns the response matching the input to the dictionary. If no match is found <code>No match found!</code>
+     * is returned.
+     */
+    public String getReponse(String input) {
+        input = input.replace("%20", " ");
+
+        responseFound = false;
+
+        for (String keyword : DICTIONARYHANDLER.getDictionary().keySet()) {
+
+            if (input.matches(keyword)) {
+
+                response = DICTIONARYHANDLER.getDictionary().get(keyword);
+
+                responseFound = true;
+
+                LOG.info("Match found for " + keyword);
+            }
+        }
+
+        if (responseFound) {
+            return response;
+        } else {
+            return "No match found!";
+        }
+    }
+
+}
