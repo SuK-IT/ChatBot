321aa6d54fa3c8a1572719e4ab2b3e1cb65496d9
Griefed <griefed@griefed.de>
2021-09-06 19:36:05 +0200

    test: Refactor copy-method to FileUtils.copyFile() instead of createFile from Main

--- backend/test/java/de/sukit/chatbot/DictionaryHandlerTest.java
+++ backend/test/java/de/sukit/chatbot/DictionaryHandlerTest.java
@@ -1,12 +1,28 @@
+/* Copyright (C) 2021  SUK-IT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package de.sukit.chatbot;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class DictionaryHandlerTest {
 
@@ -14,9 +30,10 @@ public class DictionaryHandlerTest {
 
     DictionaryHandlerTest() {
         try {
-            Files.createDirectories(Paths.get("./data"));
-        } catch (IOException ignored) {}
-        Main.createFile(new File("dictionary.json"), new File ("./data"));
+            FileUtils.copyFile(new File("backend/test/resources/dictionary.json"),new File("./data/dictionary.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.DICTIONARYHANDLER = new DictionaryHandler();
     }
 
