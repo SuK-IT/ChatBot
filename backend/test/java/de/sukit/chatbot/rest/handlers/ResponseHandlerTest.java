7feceeb9c607f5bcffded1a3a9623ab59475ae3c
Griefed <griefed@griefed.de>
2021-09-06 19:36:31 +0200

    test: Split test to make debugging easier. Refactor copy-method to FileUtils.copyFile() instead of createFile from Main

--- backend/test/java/de/sukit/chatbot/rest/handlers/ResponseHandlerTest.java
+++ backend/test/java/de/sukit/chatbot/rest/handlers/ResponseHandlerTest.java
@@ -1,14 +1,29 @@
+/* Copyright (C) 2021  SUK-IT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 package de.sukit.chatbot.rest.handlers;
 
 import de.sukit.chatbot.DictionaryHandler;
-import de.sukit.chatbot.Main;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class ResponseHandlerTest {
@@ -19,16 +34,17 @@ public class ResponseHandlerTest {
 
     ResponseHandlerTest() {
         try {
-            Files.createDirectories(Paths.get("./data"));
-        } catch (IOException ignored) {}
-        Main.createFile(new File("dictionary.json"), new File ("./data"));
+            FileUtils.copyFile(new File("backend/test/resources/dictionary.json"),new File("./data/dictionary.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.DICTIONARYHANDLER = new DictionaryHandler();
         this.DICTIONARY = DICTIONARYHANDLER.getDictionary();
         this.RESPONSEHANDLER = new ResponseHandler(DICTIONARYHANDLER);
     }
 
     @Test
-    void responseTest() {
+    void responseGetCompareDictionaryTest() {
         Assertions.assertEquals(DICTIONARY.get("wort0"), RESPONSEHANDLER.getReponse("wort0"));
         Assertions.assertEquals(DICTIONARY.get("wort1"), RESPONSEHANDLER.getReponse("wort1"));
         Assertions.assertEquals(DICTIONARY.get("wort2"), RESPONSEHANDLER.getReponse("wort2"));
@@ -39,6 +55,10 @@ public class ResponseHandlerTest {
         Assertions.assertEquals(DICTIONARY.get("wort7"), RESPONSEHANDLER.getReponse("wort7"));
         Assertions.assertEquals(DICTIONARY.get("wort8"), RESPONSEHANDLER.getReponse("wort8"));
         Assertions.assertEquals(DICTIONARY.get("wort9"), RESPONSEHANDLER.getReponse("wort9"));
+    }
+
+    @Test
+    void responseGetCompareStringTest() {
         Assertions.assertEquals("1 - Hallo Welt!", RESPONSEHANDLER.getReponse("wort0"));
         Assertions.assertEquals("2 - Hallo Welt!", RESPONSEHANDLER.getReponse("wort1"));
         Assertions.assertEquals("3 - Hallo Welt!", RESPONSEHANDLER.getReponse("wort2"));
