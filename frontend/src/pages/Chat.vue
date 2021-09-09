d2c94c33b040be9c2e0b1ace8b3632c2495a0a9d
Nico <corvex166@gmail.com>
2021-09-09 10:34:41 +0200

    adjusted linking for production use

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -45,9 +45,7 @@ import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 import { QScrollArea } from 'quasar'
 
-const TEST_URL_2 = 'https://dev.suk-it.griefed.de/api/talk?input='
-const TEST_URL = '/api/talk?input=';
-const REQUEST_URL = '/api/talk/getResponse?input=';
+const REQUEST_URL = '/api/talk?input=';
 const MESSAGE_ME_HTML = '<div class="q-message q-message-sent"><div class="q-message-container row items-end no-wrap reverse"><div class=""><div class="q-message-name q-message-name--sent">$MESSAGESENDER</div><div class="q-message-text q-message-text--sent"><div class="q-message-text-content q-message-text-content--sent"><div>$MESSAGETEXT</div></div></div></div></div></div>';
 const MESSAGE_RECEIVED_HTML = '<div class="q-message q-message-received"><div class="q-message-container row items-end no-wrap"><div class=""><div class="q-message-name q-message-name--received">$MESSAGESENDER</div><div class="q-message-text q-message-text--received"><div class="q-message-text-content q-message-text-content--received"><div>$MESSAGETEXT</div></div></div></div></div></div>';
 
@@ -101,7 +99,7 @@ export class MessageHelper
   {
     let escaped = this.escapeInput(text, true);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    void axios.get(TEST_URL_2 + escaped).then(response => { this.AddMessage(response.data, false); })
+    void axios.get(REQUEST_URL + escaped).then(response => { this.AddMessage(response.data, false); })
     .catch(function (error) { alert(error); });
   }
 
